079e9e505ff18a18b608ce7f8324f1d9840b003f
Dave Fishel <dave.fishel@gmail.com>
2020-06-22 22:38:51 -0700

    Initial Commit

---
+++ SplitCaster/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SplitCaster
+//
+//  Created by Dave Fishel on 6/22/20.
+//  Copyright © 2020 Dave Fishel. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
